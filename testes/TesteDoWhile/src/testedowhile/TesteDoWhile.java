de80fa0247d023d62cf4f143cea15e556405863a
Gabriel Dias <gabrielmdais557@gmail.com>
2024-08-12 01:39:57 -0300

    git desgraçado

---
+++ testes/TesteDoWhile/src/testedowhile/TesteDoWhile.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testedowhile;
+
+/**
+ *
+ * @author gabri
+ */
+public class TesteDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            i++;
+            System.out.println(i + "° loop");
+        } while (i < 5);
+
+    }
+
+}
